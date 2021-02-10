5441bc8522faee3d518fcd8ef086123622e9b00b
Jose Miguel C Evangelista <jomigevangelista117@gmail.com>
2021-02-10 20:15:19 +0800

    Push to github -initial commit

---
+++ getElementByClassName.js
@@ -0,0 +1,67 @@
+// document.getElementById();
+
+// console.log(document.getElementById('task-title'));
+
+// const taskTitle = document.getElementById('task-title');
+
+// taskTitle.style.background = 'green';
+// taskTitle.style.color = '#002';
+// taskTitle.style.padding = '20px';
+// taskTitle.style.fontStyle = 'italic';
+
+// taskTitle.textContent = 'Wow';
+// taskTitle.innerText = 'My Crib';
+// taskTitle.innerHTML = '<h1 style="color: white">Task List</h1>';
+
+
+//document.getElementsByClassName const items = document.getElementsByClassName('collection-item');
+console.log(items);
+
+console.log(items[0]);
+items[0].style.color = 'blue';
+items[0].textContent = "UNO";
+
+const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+console.log(listItems);
+
+//document.getElementByTagName();
+ let just = document.getElementsByTagName('li');
+ console.log(just);
+ console.log(just[0]);
+ just[0].style.color = 'orange';
+ just[0].textContent = 'power';
+
+// Convert HTML Collection to Array
+just = Array.from(just);
+just.reverse();
+just.forEach(function(just, index){
+  console.log(just.className);
+  just.textContent = `${index}: JOMIG`;
+});
+
+
+console.log(just);
+
+// document.querySelectorAll()
+const jomigs = document.querySelectorAll('ul.collection li.collection-item');
+
+console.log(jomigs);
+
+nayeons.forEach(function(nayeon, index){
+    jomigs.textContent = `${index}: JUSTJOMIG`;
+});
+
+const liOdd = document.querySelectorAll('li:nth-child(odd)')
+const liEven = document.querySelectorAll('li:nth-child(even)')
+
+liOdd.forEach(function(li){
+  li.style.background = 'yellow';
+  li.style.color = '#ccc';
+})
+
+for(let a = 0; a < liEven.length; a++) {
+    liEven[a].style.background = 'orange'
+}
+
+
+
